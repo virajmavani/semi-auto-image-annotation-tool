daabbf8a5f59b966b1558b59d29d1d64c6abc90d
Viraj Mavani <viraj.mavani1996@gmail.com>
2018-06-12 16:18:05 +0530

    resolved bug and updated requirements.txt

--- main.py
+++ main.py
@@ -128,6 +128,8 @@ class MainGUI:
         self.canvas.bind("<B1-Motion>", self.mouse_drag)
         self.canvas.bind("<ButtonRelease-1>", self.mouse_release)
         self.parent.bind("Escape", self.cancel_bbox)
+        self.parent.bind("<Key-Left>", self.open_previous)
+        self.parent.bind("<Key-Right>", self.open_next)
 
         # Labels and Bounding Box Lists Panel
         self.listPanel = Frame(self.frame)
@@ -166,7 +168,7 @@ class MainGUI:
         self.imageDir = filedialog.askdirectory(title="Select Dataset Directory")
         self.imageList = os.listdir(self.imageDir)
         self.imageList = sorted(self.imageList)
-        # print(self.imageList)
+        self.filename = None
         self.load_image(self.imageDir + '/' + self.imageList[self.cur])
 
     def load_image(self, file):
